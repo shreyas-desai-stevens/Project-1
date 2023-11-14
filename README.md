8340a4c6e05e3ede695f00858e2cbaac1fb7298f
EP-shreyas <shreyas.desai@extrapreneursindia.com>
2023-11-14 11:06:22 -0500

    updated gron flags

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-R
+Added Extensions:
+
+1.added .err files to check if there is error in the input and compare it with .err files rather than .out files
+
+2.added flag checks for wc.py, -l,-w,-c
+
+3.added --object flag for gron.py to get a base object name for json
--- prog/gron.py
+++ prog/gron.py
@@ -2,33 +2,37 @@ import argparse
 import json
 import sys
 
-def gron(json_data, parent_key='json', depth=0):
+def gron(json_data, base_obj='json', depth=0):
     result = []
 
     if isinstance(json_data, dict):
-        result.append(f"{parent_key} = {{}};")
+        result.append(f"{base_obj} = {{}};")
         for key, value in sorted(json_data.items()):
-            new_key = f"{parent_key}.{key}" if parent_key else key
+            new_key = f"{base_obj}.{key}" if base_obj else key
             result.extend(gron(value, new_key, depth=depth + 1))
     elif isinstance(json_data, list):
-        result.append(f"{parent_key} = [];")
+        result.append(f"{base_obj} = [];")
         for i, item in enumerate(json_data):
-            new_key = f"{parent_key}[{i}]"
+            new_key = f"{base_obj}[{i}]"
             result.extend(gron(item, new_key, depth=depth + 1))
     else:
-        result.append(f"{parent_key} = {json.dumps(json_data)};")
+        result.append(f"{base_obj} = {json.dumps(json_data)};")
 
     return [f"{line}" for line in result]
 
 def main():
     parser = argparse.ArgumentParser(description="Return a JSON file structure in a flattened way")
     parser.add_argument("input_json", nargs='?', type=argparse.FileType("r"), default=sys.stdin, help="File to flatten JSON from (default is stdin)")
+    parser.add_argument("-o","--object",nargs='?',help="Add a base object of your choice (default is 'json')")
     args = parser.parse_args()
     try:
         with args.input_json as json_file:
             json_data = json.load(json_file)
 
-        result_lines = gron(json_data)
+        if args.object:
+            result_lines = gron(json_data,base_obj=args.object)
+        else:
+            result_lines = gron(json_data)
 
         for line in result_lines:
             print(line)
